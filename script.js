bc7499f3198b3cc38e24acf5e299d46822b63e35
alisherkhann <sherkhanali032@gmail.com>
2024-11-07 19:26:08 +0530

    added buttons

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const images = document.querySelectorAll('.container img');
 const progress = document.querySelector('.progress_bar');
+const leftButton = document.querySelector('.left-button');
+const rightButton = document.querySelector('.right-button');
 
 let timeLimit = 5000;
 let index = 0;
 
-let nextSlide = () => {
-    index = (index + 1) % images.length;
-
+let updateSlide = () => {
     progress.style.transition = 'none'; 
-    progress.style.width = '0%'; // Set width to 0%
+    progress.style.width = '0%'; 
 
-    // Use setTimeout to delay the transition
     setTimeout(() => {
-        progress.style.transition = `width ${timeLimit / 1000}s linear`; // Set transition for width
-        progress.style.width = '70%'; // Set width to 70%
+        progress.style.transition = `width ${timeLimit / 1000}s linear`; 
+        progress.style.width = '70%'; 
     }, 50);
 
     images.forEach(img => {
@@ -21,8 +20,34 @@ let nextSlide = () => {
     });
 }
 
-setInterval(nextSlide, timeLimit);
+let slideTransition = (direction) => {
+    clearInterval(interval);
+    
+    if (direction === 'right') {
+        index = (index + 1) % images.length; 
+    } else if (direction === 'left') {
+        index = (index - 1 + images.length) % images.length; 
+    }
 
-// Start the progress bar for the first slide
+    updateSlide();
+    interval = setInterval(() => {
+        index = (index + 1) % images.length; 
+        updateSlide();
+    }, timeLimit);
+}
+
+let interval = setInterval(() => {
+    index = (index + 1) % images.length; 
+    updateSlide();
+}, timeLimit);
+
+rightButton.addEventListener('click', () => {
+    slideTransition('right'); 
+});
+
+leftButton.addEventListener('click', () => {
+    slideTransition('left'); 
+});
+
 progress.style.transition = 'width 5s linear';
 progress.style.width = '70%';
